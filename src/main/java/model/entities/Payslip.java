82154755f420ea3206ffccbd263f22cf3356edc6
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-12-06 10:11:14 -0300

    feat: criada a classe Payslip

--- src/main/java/model/entities/Payslip.java
+++ src/main/java/model/entities/Payslip.java
@@ -1,4 +1,34 @@
 package model.entities;
 
+import java.math.BigDecimal;
+
 public class Payslip {
+  private BigDecimal inss;
+  private BigDecimal irrf;
+  private BigDecimal netSalary;
+
+  private Employee employee;
+
+  public Payslip(BigDecimal inss, BigDecimal irrf, BigDecimal netSalary, Employee employee) {
+    this.inss = inss;
+    this.irrf = irrf;
+    this.netSalary = netSalary;
+    this.employee = employee;
+  }
+
+  public BigDecimal getInss() {
+    return inss;
+  }
+
+  public BigDecimal getIrrf() {
+    return irrf;
+  }
+
+  public BigDecimal getNetSalary() {
+    return netSalary;
+  }
+
+  public Employee getEmployee() {
+    return employee;
+  }
 }
