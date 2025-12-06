508ae3733773f8f049263fe15cc8e59e3bee2de0
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-12-06 15:03:27 -0300

    refactor: refatoramento na logica de calculo progressivo do INSS

--- src/main/java/service/calculations/InssCalculationService.java
+++ src/main/java/service/calculations/InssCalculationService.java
@@ -1,51 +1,66 @@
 package service.calculations;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class InssCalculationService {
+  private static final BigDecimal BAND_LIMIT_1 = BigDecimal.valueOf(1412);
+  private static final BigDecimal BAND_LIMIT_2 = BigDecimal.valueOf(2666.68);
+  private static final BigDecimal BAND_LIMIT_3 = BigDecimal.valueOf(4000.03);
+  private static final BigDecimal BAND_LIMIT_4 = BigDecimal.valueOf(7786.02);
+
+  private static final BigDecimal TAX_RANGE_1 = BigDecimal.valueOf(0.075);
+  private static final BigDecimal TAX_RANGE_2 = BigDecimal.valueOf(0.09);
+  private static final BigDecimal TAX_RANGE_3 = BigDecimal.valueOf(0.12);
+  private static final BigDecimal TAX_RANGE_4 = BigDecimal.valueOf(0.14);
+
   public BigDecimal calculate(BigDecimal grossSalary) {
-    if (grossSalary.compareTo(BigDecimal.valueOf(1412)) <= 0) {
-      return grossSalary.multiply(BigDecimal.valueOf(0.075));
+    if (grossSalary.compareTo(BAND_LIMIT_1) <= 0) {
+      BigDecimal result = grossSalary.multiply(TAX_RANGE_1);
+      return result.setScale(2, RoundingMode.HALF_UP);
     }
-    else if (grossSalary.compareTo(BigDecimal.valueOf(1412)) > 0 && grossSalary.compareTo(BigDecimal.valueOf(2666.68)) <= 0) {
-      BigDecimal taxOnfirstPart = BigDecimal.valueOf(1412).multiply(BigDecimal.valueOf(0.075));
+    else if (grossSalary.compareTo(BAND_LIMIT_1) > 0 && grossSalary.compareTo(BAND_LIMIT_2) <= 0) {
+      BigDecimal taxOnfirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
 
-      BigDecimal restPart = grossSalary.subtract(BigDecimal.valueOf(1412));
+      BigDecimal restPart = grossSalary.subtract(BAND_LIMIT_1);
 
-      BigDecimal taxOnRestPart = restPart.multiply(BigDecimal.valueOf(0.09));
+      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_2);
 
-      return taxOnfirstPart.add(taxOnRestPart);
+      BigDecimal result = taxOnfirstPart.add(taxOnRestPart);
+      return result.setScale(2, RoundingMode.HALF_UP);
     }
-    else if (grossSalary.compareTo(BigDecimal.valueOf(2666.68)) > 0 && grossSalary.compareTo(BigDecimal.valueOf(4000.03)) <= 0) {
-      BigDecimal taxOnfirstPart = BigDecimal.valueOf(1412).multiply(BigDecimal.valueOf(0.075));
+    else if (grossSalary.compareTo(BAND_LIMIT_2) > 0 && grossSalary.compareTo(BAND_LIMIT_3) <= 0) {
+      BigDecimal taxOnfirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
 
-      BigDecimal secondPart = BigDecimal.valueOf(2666.68).subtract(BigDecimal.valueOf(1412));
+      BigDecimal secondPart = BAND_LIMIT_2.subtract(BAND_LIMIT_1);
 
-      BigDecimal taxOnSecondPart = secondPart.multiply(BigDecimal.valueOf(0.09));
+      BigDecimal taxOnSecondPart = secondPart.multiply(TAX_RANGE_2);
 
-      BigDecimal restPart = grossSalary.subtract(BigDecimal.valueOf(2666.68));
+      BigDecimal restPart = grossSalary.subtract(BAND_LIMIT_2);
 
-      BigDecimal taxOnRestPart = restPart.multiply(BigDecimal.valueOf(0.12));
+      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_3);
 
-      return taxOnfirstPart.add(taxOnSecondPart).add(taxOnRestPart);
+      BigDecimal result = taxOnfirstPart.add(taxOnSecondPart).add(taxOnRestPart);
+      return result.setScale(2, RoundingMode.HALF_UP);
     } else {
       BigDecimal baseCalculation = grossSalary;
-      if (grossSalary.compareTo(BigDecimal.valueOf(7786.02)) > 0) {
-        baseCalculation = BigDecimal.valueOf(7786.02);
+      if (grossSalary.compareTo(BAND_LIMIT_4) > 0) {
+        baseCalculation = BAND_LIMIT_4;
       }
 
-      BigDecimal taxOnFirstPart = BigDecimal.valueOf(1412).multiply(BigDecimal.valueOf(0.075));
+      BigDecimal taxOnFirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
 
-      BigDecimal range2Diff = BigDecimal.valueOf(2666.68).subtract(BigDecimal.valueOf(1412));
-      BigDecimal taxOnSecondPart = range2Diff.multiply(BigDecimal.valueOf(0.09));
+      BigDecimal range2Diff = BAND_LIMIT_2.subtract(BAND_LIMIT_1);
+      BigDecimal taxOnSecondPart = range2Diff.multiply(TAX_RANGE_2);
 
-      BigDecimal range3Diff = BigDecimal.valueOf(4000.03).subtract(BigDecimal.valueOf(2666.68));
-      BigDecimal taxOnThirdPart = range3Diff.multiply(BigDecimal.valueOf(0.12));
+      BigDecimal range3Diff = BAND_LIMIT_3.subtract(BAND_LIMIT_2);
+      BigDecimal taxOnThirdPart = range3Diff.multiply(TAX_RANGE_3);
 
-      BigDecimal restPart = baseCalculation.subtract(BigDecimal.valueOf(4000.03));
-      BigDecimal taxOnRestPart = restPart.multiply(BigDecimal.valueOf(0.14));
+      BigDecimal restPart = baseCalculation.subtract(BAND_LIMIT_3);
+      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_4);
 
-      return taxOnFirstPart.add(taxOnRestPart).add(taxOnThirdPart).add(taxOnRestPart);
+      BigDecimal result = taxOnFirstPart.add(taxOnSecondPart).add(taxOnThirdPart).add(taxOnRestPart);
+      return result.setScale(2, RoundingMode.HALF_UP);
     }
   }
 }
