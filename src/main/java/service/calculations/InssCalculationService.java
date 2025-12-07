4e01546720edfc6c2f001dc61a92aad5d86e769d
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-12-07 18:48:03 -0300

    reafactor: troca na formula de calculo do INSS para simplificacao

--- src/main/java/service/calculations/InssCalculationService.java
+++ src/main/java/service/calculations/InssCalculationService.java
@@ -4,62 +4,35 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class InssCalculationService {
+  // TABELA INSS 2025
   private static final BigDecimal BAND_LIMIT_1 = BigDecimal.valueOf(1518);
   private static final BigDecimal BAND_LIMIT_2 = BigDecimal.valueOf(2793.88);
   private static final BigDecimal BAND_LIMIT_3 = BigDecimal.valueOf(4190.83);
-  private static final BigDecimal BAND_LIMIT_4 = BigDecimal.valueOf(8157.41);
 
   private static final BigDecimal TAX_RANGE_1 = BigDecimal.valueOf(0.075);
   private static final BigDecimal TAX_RANGE_2 = BigDecimal.valueOf(0.09);
   private static final BigDecimal TAX_RANGE_3 = BigDecimal.valueOf(0.12);
   private static final BigDecimal TAX_RANGE_4 = BigDecimal.valueOf(0.14);
 
+  private static final BigDecimal DEDUCTION_1 = BigDecimal.valueOf(0);
+  private static final BigDecimal DEDUCTION_2 = BigDecimal.valueOf(22.77);
+  private static final BigDecimal DEDUCTION_3 = BigDecimal.valueOf(106.59);
+  private static final BigDecimal DEDUCTION_4 = BigDecimal.valueOf(190.40);
+
   public BigDecimal calculate(BigDecimal grossSalary) {
     if (grossSalary.compareTo(BAND_LIMIT_1) <= 0) {
-      BigDecimal result = grossSalary.multiply(TAX_RANGE_1);
+      BigDecimal result = grossSalary.multiply(TAX_RANGE_1).subtract(DEDUCTION_1);
       return result.setScale(2, RoundingMode.HALF_UP);
     }
     else if (grossSalary.compareTo(BAND_LIMIT_1) > 0 && grossSalary.compareTo(BAND_LIMIT_2) <= 0) {
-      BigDecimal taxOnfirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
-
-      BigDecimal restPart = grossSalary.subtract(BAND_LIMIT_1);
-
-      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_2);
-
-      BigDecimal result = taxOnfirstPart.add(taxOnRestPart);
+      BigDecimal result = grossSalary.multiply(TAX_RANGE_2).subtract(DEDUCTION_2);
       return result.setScale(2, RoundingMode.HALF_UP);
     }
     else if (grossSalary.compareTo(BAND_LIMIT_2) > 0 && grossSalary.compareTo(BAND_LIMIT_3) <= 0) {
-      BigDecimal taxOnfirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
-
-      BigDecimal secondPart = BAND_LIMIT_2.subtract(BAND_LIMIT_1);
-
-      BigDecimal taxOnSecondPart = secondPart.multiply(TAX_RANGE_2);
-
-      BigDecimal restPart = grossSalary.subtract(BAND_LIMIT_2);
-
-      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_3);
-
-      BigDecimal result = taxOnfirstPart.add(taxOnSecondPart).add(taxOnRestPart);
+      BigDecimal result = grossSalary.multiply(TAX_RANGE_3).subtract(DEDUCTION_3);
       return result.setScale(2, RoundingMode.HALF_UP);
     } else {
-      BigDecimal baseCalculation = grossSalary;
-      if (grossSalary.compareTo(BAND_LIMIT_4) > 0) {
-        baseCalculation = BAND_LIMIT_4;
-      }
-
-      BigDecimal taxOnFirstPart = BAND_LIMIT_1.multiply(TAX_RANGE_1);
-
-      BigDecimal range2Diff = BAND_LIMIT_2.subtract(BAND_LIMIT_1);
-      BigDecimal taxOnSecondPart = range2Diff.multiply(TAX_RANGE_2);
-
-      BigDecimal range3Diff = BAND_LIMIT_3.subtract(BAND_LIMIT_2);
-      BigDecimal taxOnThirdPart = range3Diff.multiply(TAX_RANGE_3);
-
-      BigDecimal restPart = baseCalculation.subtract(BAND_LIMIT_3);
-      BigDecimal taxOnRestPart = restPart.multiply(TAX_RANGE_4);
-
-      BigDecimal result = taxOnFirstPart.add(taxOnSecondPart).add(taxOnThirdPart).add(taxOnRestPart);
+      BigDecimal result = grossSalary.multiply(TAX_RANGE_4).subtract(DEDUCTION_4);
       return result.setScale(2, RoundingMode.HALF_UP);
     }
   }
